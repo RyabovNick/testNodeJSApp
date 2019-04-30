b05ee7a9e7e7b0848026ab2e2f702d9731e4f2cf
123 <nikitos.aka.zed@gmail.com>
2019-04-30 15:41:24 +0300

    post, del

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const pool = require('./config/config')
+const bodyParser = require('body-parser')
 
-console.log(process.env)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.route('/api/students').post((request, response) => {
+    let { name, surname } = request.body
+
+    pool.query('Insert into `students` (name, surname) Values (?,?)',
+    [name, surname], (err, result) => {
+        console.log(err)
+        response.send(result)
+    })
+})
+
+app.route('/api/students/:n_z').delete((request, response) => {
+    pool.query('Delete from students where n_z = ?', [request.params.n_z],
+    (err, result) => {
+        console.log(err)
+        response.send(result)
+    })
+})
 
 app.route('/').get((request, response) => {
     response.send('Hello world!')
@@ -22,7 +42,7 @@ app.route('/api/students').get((req, res) => {
 })
 
 app.route('/api/students/:n_z').get((req, res) => {
-    let n_z = req.params["n_z"]
+    let n_z = req.params.n_z
     pool.query('Select * from students where n_z = ?', [n_z],
     (err,result,fields) => {
         if (err) throw err
